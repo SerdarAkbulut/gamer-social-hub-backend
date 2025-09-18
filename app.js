2d100ac047b550e5ba603d7398515d1484dff35b
SerdarAkbulut <AkbulutSerdar@outlook.com.tr>
2025-09-18 17:14:00 +0300

    ...

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const serverless = require("serverless-http");
 const sequelize = require("./src/startup/db.js");
-const app = express();
 require("./src/models/index.js");
+
 dotenv.config();
 
+const app = express();
 app.use(express.json());
 
 require("./src/startup/routers")(app);
 
-const PORT = process.env.PORT || 3000;
-
 sequelize
   .authenticate()
-  .then(() => {
-    console.log("✅ Veritabanı bağlantısı başarılı.");
-  })
-  .catch((error) => {
-    console.error("❌ Bağlantı hatası:", error);
-  });
+  .then(() => console.log("✅ Veritabanı bağlantısı başarılı."))
+  .catch((err) => console.error("❌ Bağlantı hatası:", err));
 
 sequelize
   .sync({ alter: true })
-  .then(() => {
-    console.log("✅ Veritabanı senkronizasyonu tamamlandı!");
-    app.listen(PORT, () => {
-      console.log(`🚀 Sunucu ${PORT} portunda çalışıyor.`);
-    });
-  })
-  .catch((error) => {
-    console.error("❌ Senkronizasyon hatası:", error);
+  .then(() => console.log("✅ Veritabanı senkronizasyonu tamamlandı!"))
+  .catch((err) => console.error("❌ Senkronizasyon hatası:", err));
+
+// Local için: app.listen
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`🚀 Sunucu ${PORT} portunda çalışıyor.`);
   });
+}
+
+module.exports = app;
+module.exports.handler = serverless(app);
