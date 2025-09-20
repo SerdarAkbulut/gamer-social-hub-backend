e3e8e85678d0b9d1585d009815552f4129e88c41
SerdarAkbulut <AkbulutSerdar@outlook.com.tr>
2025-09-20 14:16:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const serverless = require("serverless-http");
 const sequelize = require("./src/startup/db.js");
 require("./src/models/index.js");
 
 dotenv.config();
-
 const app = express();
 app.use(express.json());
-
 require("./src/startup/routers")(app);
 
 sequelize
@@ -16,13 +13,7 @@ sequelize
   .then(() => console.log("✅ Veritabanı bağlantısı başarılı."))
   .catch((err) => console.error("❌ Bağlantı hatası:", err));
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 Sunucu ${PORT} portunda çalışıyor`));
+
 module.exports = app;
-
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () =>
-    console.log(`🚀 Local sunucu ${PORT} portunda çalışıyor`)
-  );
-}
-
-module.exports = serverless(app);
